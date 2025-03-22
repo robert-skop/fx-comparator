60ceb8a6f3257f10afe7d9961e498ce3cfd226de
Róbert Škop <robert.skop@gmail.com>
2025-03-22 21:33:17 +0100

    feat: add basic auth

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # FX Comparator
-Compares FX rates from [CNB](https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt) with [Frankfurter API](https://frankfurter.dev/).
+
+Compares FX rates from [CNB](https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt)
+with [Frankfurter API](https://frankfurter.dev/).
+
+# Basic information
+
+The application has two secured endpoints (see the OpenAPI documentation for details) protected by Basic Auth. You can 
+configure the username and password at application startup. If no credentials are provided, default values will be used 
+(**admin:password**). Health check endpoints are not secured.
 
 # Docker image build
-Use command in project root: docker build --platform linux/amd64 -t fx-comparator .
 
-Run docker image: docker run --platform linux/amd64 -p 8080:8080 -e "CUSTOM_ENV_PROPERTY1=CUSTOM_VALUE1" -e "CUSTOM_ENV_PROPERTY2=CUSTOM_VALUE2" --name fx-comparator fx-comparator
+Use command in project root: `docker build --platform linux/amd64 -t fx-comparator .`
 
-TODO vystavit este port 8088 pre actuator
+Run docker image (**replace CUSTOM_USERNAME and CUSTOM_PASSWORD with your required credentials**):
+`docker run --platform linux/amd64 -p 8080:8080 -p 8088:8088 -e "USERNAME=CUSTOM_USERNAME" -e "PASSWORD=CUSTOM_PASSWORD" --name fx-comparator fx-comparator`
---
+++ fx-comparator-core/src/main/kotlin/com/robertskop/fxcomparator/config/SecurityConfig.kt
@@ -0,0 +1,49 @@
+package com.robertskop.fxcomparator.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    @Value("\${auth.username}") private val username: String,
+    @Value("\${auth.password}") private val password: String,
+) {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests { requests ->
+                requests
+                    .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .httpBasic(Customizer.withDefaults())
+            .csrf { it.disable() }
+        return http.build()
+    }
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val user = User
+            .withUsername(username)
+            .password(passwordEncoder().encode(password))
+            .build()
+        return InMemoryUserDetailsManager(user)
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+}
